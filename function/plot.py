3e2d58d072780b45bb1a21ab95dcf15d96a4250a
Shuaifei666 <yifzhang@unc.edu>
2024-11-17 02:03:34 +0000

    Updated files in function folder

--- function/plot.py
+++ function/plot.py
@@ -1,15 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import argparse
-import torch
-import torch.nn.functional as F
-from torch import nn, optim
-from torchvision import datasets, transforms
-from tqdm import tqdm
 from scipy.io import loadmat
-from torch.autograd import Variable
 import numpy as np
-import torch.utils.data as utils
 from torchvision.utils import save_image
 import matplotlib.pyplot as plt
 dat_mat = loadmat('./data/HCP_subcortical_CMData_desikan.mat')
@@ -17,13 +9,10 @@ tensor = dat_mat['loaded_tensor_sub']
 A_mat = np.mean(np.squeeze(tensor[:,:,1,:]), axis=2)
 A_mat = A_mat + A_mat.transpose()
 for i in range(10):
-    # 提取第 i 个样本的矩阵
-    data_matrix = tensor[:, :, 1, i]  # 第三维度为 1，样本索引为 i
-    # 对矩阵进行对称处理（如果需要）
+    data_matrix = tensor[:, :, 1, i]  
     data_matrix = data_matrix + data_matrix.transpose()
-    # 绘制并保存图像
-    plt.imshow(data_matrix)  # 您可以选择其他 colormap，例如 'gray'
+    plt.imshow(data_matrix)  
     plt.colorbar()
     plt.title(f"Sample {i+1}")
     plt.savefig(f'results/sample_{i+1}.png')
-    plt.clf()  # 清除当前图像，准备绘制下一个
+    plt.clf()  
